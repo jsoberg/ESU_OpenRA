1fa0fd8c59982a97e86936316a96d56b1725ff9e
Joshua Soberg <j15soberg@gmail.com>
2016-05-31 17:29:33 -0400

    #1 Made the Simple AI transform its MCV.

--- OpenRA.Mods.Common/AI/SimpleTestAI.cs
+++ OpenRA.Mods.Common/AI/SimpleTestAI.cs
@@ -10,31 +10,60 @@ namespace OpenRA.Mods.Common.AI
     public sealed class SimpleTestAI : ITick, IBot, INotifyDamage
     {
         private readonly SimpleTestAIInfo info;
+        private readonly World world;
+
+        private bool isEnabled;
+        private int tickCount;
 
         public SimpleTestAI(SimpleTestAIInfo info, ActorInitializer init)
         {
             this.info = info;
+            this.world = init.World;
         }
 
-        void ITick.Tick(Actor self)
-        {
-            
-        }
-
-        void IBot.Activate(Player p)
-        {
-
-        }
 
         IBotInfo IBot.Info
         {
             get { return info; }
         }
 
+        void IBot.Activate(Player p)
+        {
+            isEnabled = true;
+        }
+
         void INotifyDamage.Damaged(Actor self, AttackInfo e)
         {
 
         }
+
+        void ITick.Tick(Actor self)
+        {
+            if (!isEnabled)
+                return;
+
+            tickCount++;
+
+            if (tickCount == 1) {
+                DeployMcv(self);
+            }
+            
+        }
+
+        void DeployMcv(Actor self)
+        {
+            var mcv = world.Actors.FirstOrDefault(a => a.Owner == self.Owner && a.Info.Name == "mcv");
+
+            if (mcv != null)
+            {
+                world.IssueOrder(new Order("DeployTransform", mcv, true));
+            }
+            else
+            {
+                throw new ArgumentNullException("Cannot find MCV");
+            }
+        }
+
     }
 
     public sealed class SimpleTestAIInfo : IBotInfo, ITraitInfo
