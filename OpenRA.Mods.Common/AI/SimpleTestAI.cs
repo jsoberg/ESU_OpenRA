26e06d0eaff171f961c53207963e89855679aa2e
Joshua Soberg <j15soberg@gmail.com>
2016-05-31 14:44:23 -0400

    #1 - Implemented a new AI interface (SimpleTestAI) that isn't connected to HackAI. It literally does nothing at this point, but it compiles and runs so we should definitely be able to use it.

---
+++ OpenRA.Mods.Common/AI/SimpleTestAI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+/** A simple AI used for testing. */
+namespace OpenRA.Mods.Common.AI
+{
+    public sealed class SimpleTestAI : ITick, IBot, INotifyDamage
+    {
+        private readonly SimpleTestAIInfo info;
+
+        public SimpleTestAI(SimpleTestAIInfo info, ActorInitializer init)
+        {
+            this.info = info;
+        }
+
+        void ITick.Tick(Actor self)
+        {
+            
+        }
+
+        void IBot.Activate(Player p)
+        {
+
+        }
+
+        IBotInfo IBot.Info
+        {
+            get { return info; }
+        }
+
+        void INotifyDamage.Damaged(Actor self, AttackInfo e)
+        {
+
+        }
+    }
+
+    public sealed class SimpleTestAIInfo : IBotInfo, ITraitInfo
+    {
+        private const string AI_NAME = "Simple Test AI";
+
+        string IBotInfo.Name
+        {
+            get { return AI_NAME; }
+        }
+
+        object ITraitInfo.Create(ActorInitializer init)
+        {
+            return new SimpleTestAI(this, init);
+        }
+    }
+}
