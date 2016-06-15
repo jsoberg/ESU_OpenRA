a13033656abfca0234362dc35325c82d99fc6138
Joshua Soberg <j15soberg@gmail.com>
2016-06-15 18:39:49 -0400

    #4 - Added a shell IGameOver trait class where we can put our game over analysis code.

---
+++ OpenRA.Mods.Common/Traits/Esu/SignalGameOver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits.Esu
+{
+    public class SignalGameOverInfo : ITraitInfo
+    {
+        public object Create(ActorInitializer init)
+        {
+            return new SignalGameOver();
+        }
+    }
+
+    /** A simple callback to tell us when the game is over. */
+    public class SignalGameOver : IGameOver
+    {
+        public void GameOver(World world)
+        {
+            Console.WriteLine("Game Complete!");
+            Log.Write("order_manager", "Game Complete!");
+        }
+    }
+}
