949416c5e5ae3b285189002405187e680cbd7ce8
Joshua Soberg <j15soberg@gmail.com>
2016-08-07 10:13:14 -0400

    - Re-adding EsuAI files with proper naming.

---
+++ OpenRA.Mods.Common/AI/Esu/EsuAI.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+using OpenRA.Mods.Common.AI.Esu.Geometry;
+using OpenRA.Mods.Common.AI.Esu.Rules;
+
+/// <summary>
+///  This class is the implementation of the modular ESU AI, with a ruleset described at the project's <see href="https://github.com/jsoberg/ESU_OpenRA/wiki/AI-Rules">GitHub Wiki</see>.
+/// </summary>
+namespace OpenRA.Mods.Common.AI.Esu
+{
+    public sealed class EsuAI : ITick, IBot, INotifyDamage, INotifyDiscovered, INotifyOtherProduction
+    {
+        private readonly EsuAIInfo info;
+        private readonly World world;
+        private readonly StrategicWorldState worldState;
+
+        // Rulesets.
+        private readonly List<BaseEsuAIRuleset> rulesets;
+
+        private Player selfPlayer;
+        private bool isEnabled;
+        private int tickCount;
+
+        public EsuAI(EsuAIInfo info, ActorInitializer init)
+        {
+            this.info = info;
+            this.world = init.World;
+            this.worldState = new StrategicWorldState();
+
+            rulesets = new List<BaseEsuAIRuleset>();
+            addRulesets();
+        }
+
+        private void addRulesets()
+        {
+            rulesets.Add(new EsuAIBuildRuleset(world, info));
+            rulesets.Add(new EsuAIUnitRuleset(world, info));
+        }
+
+        IBotInfo IBot.Info
+        {
+            get { return info; }
+        }
+
+        void IBot.Activate(Player p)
+        {
+            isEnabled = true;
+            selfPlayer = p;
+
+            foreach (BaseEsuAIRuleset rs in rulesets) {
+                rs.Activate(p);
+            }
+        }
+
+        void INotifyDamage.Damaged(Actor self, AttackInfo e)
+        {
+
+        }
+
+        void INotifyDiscovered.OnDiscovered(Actor self, Player discoverer, bool playNotification)
+        {
+
+        }
+
+        void INotifyOtherProduction.UnitProducedByOther(Actor self, Actor producer, Actor produced)
+        {
+            var notifyOtherProductionRulesets = rulesets.Where(a => a is INotifyOtherProduction);
+            foreach (INotifyOtherProduction rs in notifyOtherProductionRulesets) {
+                rs.UnitProducedByOther(self, producer, produced);
+            }
+        }
+
+        void ITick.Tick(Actor self)
+        {
+            if (!isEnabled) {
+                return;
+            }
+
+            tickCount++;
+
+            // Check for initial tick.
+            if (tickCount == 1) {
+                DeployMcv(self);
+                return;
+            }
+
+            if (!worldState.IsInitialized) {
+                worldState.Initalize(world, selfPlayer);
+            } else {
+                worldState.UpdateCurrentWorldState();
+            }
+
+            // Get and issue orders.
+            Queue<Order> orders = new Queue<Order>();
+            foreach (BaseEsuAIRuleset rs in rulesets) {
+                rs.Tick(self, worldState, orders);
+            }
+
+            foreach (Order order in orders) {
+                world.IssueOrder(order);
+            }
+        }
+
+        private void DeployMcv(Actor self)
+        {
+            var mcv = world.Actors.FirstOrDefault(a => a.Owner == self.Owner && a.Info.Name == "mcv");
+
+            if (mcv != null) {
+                world.IssueOrder(new Order("DeployTransform", mcv, true));
+            } else {
+                throw new ArgumentNullException("Cannot find MCV");
+            }
+        }
+    }
+
+    public sealed class EsuAIInfo : IBotInfo, ITraitInfo
+    {
+        private const string AI_NAME = "ESU AI";
+
+        // ========================================
+        // Rule Tunable
+        // ========================================
+
+        [Desc("Minimum excess power we should maintain (Rule BuildPowerPlantIfBelowMinimumExcessPower)")]
+        public readonly int MinimumExcessPower = 50;
+
+        [Desc("Determines whether we should produce a scout before a refinery (Rule ShouldProduceScoutBeforeRefinery)")]
+        public readonly int ShouldProduceScoutBeforeRefinery = 1;
+
+        // ========================================
+        // Static
+        // ========================================
+        [Desc("Radius in cells around the center of the base to expand.")]
+        public readonly int MaxBaseRadius = 20;
+
+        string IBotInfo.Name
+        {
+            get { return AI_NAME; }
+        }
+
+        object ITraitInfo.Create(ActorInitializer init)
+        {
+            return new EsuAI(this, init);
+        }
+    }
+}
---
+++ OpenRA.Mods.Common/AI/Esu/EsuAIUtils.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.AI.Esu.Geometry;
+
+namespace OpenRA.Mods.Common.AI.Esu
+{
+    class EsuAIUtils
+    {
+        // ========================================
+        // Location Tasks
+        // ========================================
+
+        public static CPos OppositeBaseLocationOfPlayer(World world, Player player)
+        {
+            var constructionYard = world.Actors.Where(a => a.Owner == player &&
+                a.Info.Name == EsuAIConstants.Buildings.CONSTRUCTION_YARD).FirstOrDefault();
+            if (constructionYard == null) {
+                throw new NoConstructionYardException("Contruction yard not yet created");
+            }
+
+            var selfLocation = constructionYard.Location;
+
+            return GeometryUtils.OppositeLocationOnMap(selfLocation, world.Map);
+        }
+
+        // ========================================
+        // Production Tasks
+        // ========================================
+
+        public static bool IsAnyItemCurrentlyInProductionForCategory(World world, Player owner, string category)
+        {
+            IEnumerable<ProductionItem> items = ItemsCurrentlyInProductionQueuesForCategory(world, owner, category);
+            return (items.Count() > 0);
+        }
+
+        public static bool IsItemCurrentlyInProductionForCategory(World world, Player owner, string category, string itemName)
+        {
+            IEnumerable<ProductionItem> items = ItemsCurrentlyInProductionQueuesForCategory(world, owner, category);
+            foreach (ProductionItem item in items) {
+                if (item.Item == itemName) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static IEnumerable<ProductionItem> ItemsCurrentlyInProductionQueuesForCategory(World world, Player owner, string category)
+        {
+            IEnumerable<ProductionQueue> productionQueues = FindProductionQueues(world, owner, category);
+
+            List<ProductionItem> itemsInQueues = new List<ProductionItem>();
+            foreach (ProductionQueue queue in productionQueues) {
+                if (queue.CurrentItem() != null) {
+                    itemsInQueues.Add(queue.CurrentItem());
+                }
+            }
+
+            return itemsInQueues;
+        }
+
+        public static IEnumerable<ProductionQueue> FindProductionQueues(World world, Player owner, string category)
+        {
+            return world.ActorsWithTrait<ProductionQueue>()
+                .Where(a => a.Actor.Owner == owner && a.Trait.Info.Type == category && a.Trait.Enabled)
+                .Select(a => a.Trait);
+        }
+
+        public static bool CanBuildItemWithNameForCategory(World world, Player owner, string category, string name)
+        {
+            return world.ActorsWithTrait<ProductionQueue>()
+                .Any(pq => pq.Actor.Owner == owner && pq.Trait.Info.Type == category && pq.Trait.Enabled && pq.Trait.BuildableItems().Any(ai => ai.Name == name));
+        }
+
+        public static int BuildingCountForPlayerOfType(World world, Player owner, string buildingName)
+        {
+            return world.ActorsHavingTrait<Building>()
+                .Count(a => a.Owner == owner && a.Info.Name == buildingName);
+        }
+    }
+
+    // ========================================
+    // Various Exceptions
+    // ========================================
+
+    public class NoConstructionYardException : NullReferenceException
+    {
+        public NoConstructionYardException(String message) : base(message) { }
+    }
+}
