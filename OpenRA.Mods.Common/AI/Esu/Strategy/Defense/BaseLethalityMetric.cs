713eb2759b8e060f2c65433c31812894f66a9a97
Joshua Soberg <j15soberg@gmail.com>
2016-10-15 17:30:44 -0400

    #31 - Pushing stub for lethality metric class.

---
+++ OpenRA.Mods.Common/AI/Esu/Strategy/Defense/BaseLethalityMetric.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Mods.Common.AI.Esu.Strategy.Defense
+{
+    public class BaseLethalityMetric
+    {
+        /// <summary>
+        ///  Map describing vulnerable actors and their corresponding lethality ratings.
+        /// </summary>
+        public readonly Dictionary<Actor, int> VulnerableActorToLethalityMap;
+        /// <summary>
+        ///  Map describing offensive actors and the amount of lethality that they can cover.
+        /// </summary>
+        public readonly Dictionary<Actor, int> OffensiveActorToLethalityMap;
+
+        public BaseLethalityMetric(World world, Player selfPlayer)
+        {
+            this.VulnerableActorToLethalityMap = BuildVulnerableActorMapForPlayer(world, selfPlayer);
+            this.OffensiveActorToLethalityMap = BuildOffensiveActorMapForPlayer(world, selfPlayer);
+        }
+
+        private Dictionary<Actor, int> BuildVulnerableActorMapForPlayer(World world, Player selfPlayer)
+        {
+            //TODO stub
+            return null;
+        }
+
+        private Dictionary<Actor, int> BuildOffensiveActorMapForPlayer(World world, Player selfPlayer)
+        {
+            //TODO stub
+            return null;
+        }
+    }
+}
