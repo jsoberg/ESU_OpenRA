8e133a0005a63222fe0949575d2a5053e050938c
Joshua Soberg <j15soberg@gmail.com>
2016-10-18 19:46:48 -0400

    #32 - Stub for attack helper class.

---
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/AttackHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Mods.Common.AI.Esu.Rules.Units
+{
+    public class AttackHelper
+    {
+        private readonly World world;
+        private readonly Player selfPlayer;
+        private readonly EsuAIInfo info;
+
+        public AttackHelper(World world, Player selfPlayer, EsuAIInfo info)
+        {
+            this.world = world;
+            this.selfPlayer = selfPlayer;
+            this.info = info;
+        }
+    }
+}
