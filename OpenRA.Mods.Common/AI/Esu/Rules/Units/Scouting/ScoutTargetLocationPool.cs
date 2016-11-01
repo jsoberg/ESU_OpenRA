8d32f8c8d17cfdb69b888f8f18a45edc4bb7f4f8
Joshua Soberg <j15soberg@gmail.com>
2016-10-31 21:04:58 -0400

    - Intermediate commit, moving to laptop.

--- OpenRA.Mods.Common/AI/Esu/Rules/Units/Scouting/ScoutTargetLocationPool.cs
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/Scouting/ScoutTargetLocationPool.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenRA.Mods.Common.AI.Esu.Strategy;
 
 namespace OpenRA.Mods.Common.AI.Esu.Rules.Units
 {
-    class ScoutTargetLocationPool
+    public class ScoutTargetLocationPool
     {
+        private readonly Queue<CPos> AvailablePositions;
 
+        public ScoutTargetLocationPool()
+        {
+            this.AvailablePositions = new Queue<CPos>();
+        }
+
+        public CPos GetTargetLocationForScout(World world, StrategicWorldState state)
+        {
+            // TODO
+            return CPos.Invalid;
+        }
     }
 }
