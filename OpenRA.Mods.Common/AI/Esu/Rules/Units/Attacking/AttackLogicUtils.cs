f738d7549cb5785000c7649449da7c526438c649
Joshua Soberg <j15soberg@gmail.com>
2016-11-02 20:27:43 -0400

    #32 - Add stub for attack logic utils, for giving a discrete attack strength for a given attack.

---
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/Attacking/AttackLogicUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Mods.Common.AI.Esu.Rules.Units.Attacking
+{
+    public static class AttackLogicUtils
+    {
+        /// <summary>
+        ///  Trying to give a discrete description for the strength of any given predicted attack.
+        /// </summary>
+        public enum PredictedAttackStrength
+        {
+            None,
+            Low,
+            Medium,
+            High,
+            Overwhelming
+        };
+
+
+    }
+}
