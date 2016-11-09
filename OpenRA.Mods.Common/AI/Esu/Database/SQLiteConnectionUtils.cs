ebc398cfe90b0c116ebdcb44ea9ca09f24e36c84
Joshua Soberg <j15soberg@gmail.com>
2016-11-08 19:18:58 -0500

    #32 - Added initial code for making a connection to our db.

---
+++ OpenRA.Mods.Common/AI/Esu/Database/SQLiteConnectionUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace OpenRA.Mods.Common.AI.Esu.Database
+{
+    static class SQLiteConnectionUtils
+    {
+        private const string DatabaseFileName = "EsuAIInformation.sqlite";
+
+        private static SQLiteConnection DatabaseConnection;
+
+        static SQLiteConnection GetDatabaseConnection()
+        {
+            if (DatabaseConnection == null) {
+                DatabaseConnection =  new SQLiteConnection(DatabaseFileName);
+            }
+            return DatabaseConnection;
+        }
+    }
+}
