c2e27515b7f5b0314ac387c725d8b79daca143e9
Joshua Soberg <j15soberg@gmail.com>
2016-11-12 15:49:29 -0500

    #14 - Removed some warnings.

--- OpenRA.Mods.Common/AI/Esu/Rules/Units/Attacking/AttackHelper.cs
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/Attacking/AttackHelper.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using OpenRA.Mods.Common.Traits;
-using OpenRA.Support;
 using OpenRA.Mods.Common.AI.Esu.Strategy;
 using OpenRA.Mods.Common.AI.Esu.Strategy.Defense;
 using OpenRA.Mods.Common.AI.Esu.Strategy.Scouting;
