b17e29bc808e80511afcaf1383aa2b4d4bbeb73f
Joshua Soberg <j15soberg@gmail.com>
2016-12-13 20:30:16 -0500

    #39 - Added a class that can forward damage notifications to the ActiveAttackController.

---
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/Attacking/DamageNotifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.AI.Esu.Rules.Units.Attacking
+{
+    public class DamageNotifier : ITick, INotifyDamage
+    {
+        void ITick.Tick(Actor self)
+        {
+            // TODO Stub
+        }
+
+        void INotifyDamage.Damaged(Actor self, AttackInfo e)
+        {
+            // TODO Stub
+        }
+    }
+
+    public sealed class DamageNotifierInfo : ITraitInfo
+    {
+        public object Create(ActorInitializer init)
+        {
+            return new DamageNotifier();
+        }
+    }
+}
