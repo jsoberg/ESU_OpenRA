34c719819cbf338af180165b1d18a605dc5438c2
Joshua Soberg <j15soberg@gmail.com>
2017-01-15 21:25:49 -0500

    #54 - Added table for unit damage data.

---
+++ OpenRA.Mods.Common/AI/Esu/Database/UnitDamageDataTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace OpenRA.Mods.Common.AI.Esu.Database
+{
+    public class UnitDamageDataTable
+    {
+        private const string UnitDamageDataTableName = "UnitDamageData";
+
+        public static Column AttackingUnit = new Column("AttackingUnit", "TEXT");
+        public static Column DamagedUnit = new Column("DamagedUnit", "TEXT");
+
+        public static Column Damage = new Column("Damage", "INT");
+        public static Column WasUnitKilled = new Column("WasUnitKilled", "INT");
+
+        public static Column[] Columns = {
+            AttackingUnit, DamagedUnit, Damage, WasUnitKilled
+        };
+
+        public UnitDamageDataTable()
+        {
+            CreateTableIfNotExists();
+        }
+
+        private void CreateTableIfNotExists()
+        {
+            SQLiteConnection connection = SQLiteConnectionUtils.GetDatabaseConnection();
+            connection.Open();
+            try
+            {
+                string createTable = SQLiteUtils.GetCreateTableIfNotExistsSQLCommandString(UnitDamageDataTableName, Columns);
+                SQLiteCommand createTableCommand = new SQLiteCommand(createTable, connection);
+                createTableCommand.ExecuteNonQuery();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
