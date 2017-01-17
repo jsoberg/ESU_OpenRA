88e5ba4c7ff5dcbcb75dc89e50830870ca62de44
Joshua Soberg <j15soberg@gmail.com>
2017-01-16 22:08:30 -0500

    #54 - Move UnitDamageData class into it's own file.

---
+++ OpenRA.Mods.Common/AI/Esu/Database/UnitDamageData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.AI.Esu.Database
+{
+    public class UnitDamageData
+    {
+        public readonly string AttackerName;
+        public readonly string DamagedUnitName;
+        public readonly int Damage;
+        public readonly bool WasKilled;
+
+        public UnitDamageData(Actor damagedUnit, AttackInfo attackInfo)
+        {
+            this.AttackerName = "\"" + attackInfo.Attacker.Info.Name + "\"";
+            this.DamagedUnitName = "\"" + damagedUnit.Info.Name + "\"";
+            this.Damage = attackInfo.Damage;
+            this.WasKilled = damagedUnit.IsDead;
+        }
+    }
+}
--- OpenRA.Mods.Common/AI/Esu/Database/UnitDamageDataTable.cs
+++ OpenRA.Mods.Common/AI/Esu/Database/UnitDamageDataTable.cs
@@ -65,20 +65,4 @@ namespace OpenRA.Mods.Common.AI.Esu.Database
             }
         }
     }
-
-    public class UnitDamageData
-    {
-        public readonly string AttackerName;
-        public readonly string DamagedUnitName;
-        public readonly int Damage;
-        public readonly bool WasKilled;
-
-        public UnitDamageData(Actor damagedUnit, AttackInfo attackInfo)
-        {
-            this.AttackerName = attackInfo.Attacker.Info.Name;
-            this.DamagedUnitName = damagedUnit.Info.Name;
-            this.Damage = attackInfo.Damage;
-            this.WasKilled = damagedUnit.IsDead;
-        }
-    }
 }
