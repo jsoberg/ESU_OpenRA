c39f4b3c41d92d5ead8ee4340d55e249b016268e
Joshua Soberg <j15soberg@gmail.com>
2017-01-18 21:54:41 -0500

    #54 - Added a loader for loading unit damage data on a background thread.

---
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/CompiledUnitDamageStatistics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Mods.Common.AI.Esu.Rules.Units
+{
+    public class CompiledUnitDamageStatistics
+    {
+        private readonly Dictionary<string, DamageKillStats> UnitNameToDamageStatsMap;
+
+        public CompiledUnitDamageStatistics()
+        {
+            this.UnitNameToDamageStatsMap = new Dictionary<string, DamageKillStats>();
+        }
+
+        public void AddStatsForUnit(string attackingUnit, int damage, bool wasKill)
+        {
+            DamageKillStats stats = UnitNameToDamageStatsMap[attackingUnit];
+            if (stats == null)
+            {
+                stats = new DamageKillStats();
+            }
+            stats.Damage += damage;
+            stats.KillCount += wasKill ? 1 : 0;
+
+            UnitNameToDamageStatsMap[attackingUnit] = stats;
+        }
+    }
+
+    class DamageKillStats
+    {
+        public int Damage;
+        public int KillCount;
+    }
+}
---
+++ OpenRA.Mods.Common/AI/Esu/Rules/Units/CompiledUnitDamageStatisticsLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Mods.Common.AI.Esu.Database;
+using System.Threading;
+using System.Data.SQLite;
+
+namespace OpenRA.Mods.Common.AI.Esu.Rules.Units
+{
+    public class CompiledUnitDamageStatisticsLoader
+    {
+        private CompiledUnitDamageStatistics UnitDamageStats;
+        private readonly object StatsLock = new object();
+
+        private readonly UnitDamageDataTable UnitDamageDataTable;
+        
+        public CompiledUnitDamageStatisticsLoader()
+        {
+            this.UnitDamageDataTable = new UnitDamageDataTable();
+        }
+
+        public void ReloadUnitDamageStats()
+        {
+            var thread = new Thread(() => LoadUnitDamageStats());
+            thread.Start();
+        }
+
+        private void LoadUnitDamageStats()
+        {
+            CompiledUnitDamageStatistics stats = new CompiledUnitDamageStatistics();
+
+            SQLiteDataReader reader = UnitDamageDataTable.Query();
+            while (reader.Read()) {
+                string attackerName = (string) reader[UnitDamageDataTable.AttackingUnit.ColumnName];
+                int damage = (int)reader[UnitDamageDataTable.Damage.ColumnName];
+                bool wasKilled = ((int) reader[UnitDamageDataTable.WasUnitKilled.ColumnName]) > 0;
+
+                stats.AddStatsForUnit(attackerName, damage, wasKilled);
+            }
+
+            lock (StatsLock) {
+                UnitDamageStats = stats;    
+            }
+        }
+    }
+}
+s
